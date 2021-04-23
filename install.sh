3c9b21ad31e841d1d86158bf11597909824005f7
Samuraii <68633145+BHSamuraii@users.noreply.github.com>
2021-04-23 20:42:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo apt-get install python3-pip
 pip3 install python-nmap
+sudo pip3 install python3-nmap
 sudo apt-get upgrade python
 sudo apt-get upgrade nmap
 alias python-nmap="python3 .backup.py"

dbbd5aadad9f949060e5c7ba1b3d2c87708d9a24
Samuraii <68633145+BHSamuraii@users.noreply.github.com>
2021-05-09 16:57:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 sudo apt-get install python3-pip
 pip3 install python-nmap
 sudo pip3 install python3-nmap
 sudo apt-get upgrade python
 sudo apt-get upgrade nmap
-alias python-nmap="python3 .backup.py"
+filepath=`locate pynmap2.py`
+alias pynmap="sudo python3 $filepath"
+cd
+echo alias pynmap="sudo python3 $filepath" >> .bashrc
